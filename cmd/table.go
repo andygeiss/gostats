d7c945b280e6fd7e167a973632442dcde6a63ab2
andygeiss <4ndygeiss@gmail.com>
2019-05-19 10:43:58 +0200

    table ncr fixed

--- cmd/table.go
+++ cmd/table.go
@@ -25,13 +25,16 @@ var tableCmd = &cobra.Command{
 func printTableWithResult(result []stats.Statistics) {
 	var c, n int
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Ident", "Nodes", "Complexity"})
-	table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER})
+	table.SetHeader([]string{"Ident", "Nodes", "Complexity", "NCR"})
+	table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER})
 	for _, stat := range result {
-		table.Append([]string{stat.Ident, strconv.Itoa(stat.Nodes), strconv.Itoa(stat.Complexity)})
+		if stat.Complexity == 0 {
+			stat.Complexity++
+		}
+		table.Append([]string{stat.Ident, strconv.Itoa(stat.Nodes), strconv.Itoa(stat.Complexity), strconv.Itoa(stat.Nodes/stat.Complexity)})
 		c += stat.Complexity
 		n += stat.Nodes
 	}
-	table.SetFooter([]string{"Total", strconv.Itoa(n), strconv.Itoa(c)})
+	table.SetFooter([]string{"Total", strconv.Itoa(n), strconv.Itoa(c), strconv.Itoa(n/c)})
 	table.Render()
 }
